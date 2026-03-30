9f4733b9a4857f84f3fd100435c0364cd71a5a16
Micshoop2001 <Micshoop2001@yahoo.com>
2026-03-29 21:34:24 -0400

    jj

--- project/flask_app.py
+++ project/flask_app.py
@@ -1,5 +1,6 @@
-from application import create_app
-from application.models import db
+from project.application import create_app
+from project.application.models import db
+
 
 app = create_app('ProductionConfig')
 
