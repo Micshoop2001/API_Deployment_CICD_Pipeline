00fc7f1c8df1e4562b641d2412e33ea0b7883e8b
Micshoop2001 <Micshoop2001@yahoo.com>
2026-03-29 21:40:28 -0400

    gg

--- project/flask_app.py
+++ project/flask_app.py
@@ -1,8 +1,13 @@
-from project.application import create_app
-from project.application.models import db
+print(">>> Importing flask_app.py")
 
+from project.application import create_app
+print(">>> Imported create_app")
+
+from project.application.models import db
+print(">>> Imported db")
 
 app = create_app('ProductionConfig')
+print(">>> App created")
 
 # Only run create_all locally
 if __name__ == "__main__":
